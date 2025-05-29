86741f187fc92df756384f11aa88751a107265ea
victor <vfm@poli.br>
2025-05-29 02:45:51 +0000

    fix: fix after merge, navbar navigation

--- ma-psicologia/src/pages/Neuropsicologia/script.js
+++ ma-psicologia/src/pages/Neuropsicologia/script.js
@@ -4,3 +4,18 @@ const menu = document.getElementById("menu");
 menuIcon.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
+
+document.querySelector(".dropbtn").addEventListener("click", function (e) {
+  e.preventDefault(); // Impede comportamento padrão
+  const dropdown = this.parentElement;
+  dropdown.classList.toggle("show");
+});
+
+// Fecha o dropdown se clicar fora
+window.addEventListener("click", function (e) {
+  if (!e.target.matches(".dropbtn")) {
+    document.querySelectorAll(".dropdown").forEach(function (dropdown) {
+      dropdown.classList.remove("show");
+    });
+  }
+});
