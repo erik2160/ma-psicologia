db2b634020b2f642ff44d016f2f95766605647da
victor <vfm@poli.br>
2025-05-29 02:02:01 +0000

    feat: dropdown

--- ma-psicologia/src/pages/Home/script.js
+++ ma-psicologia/src/pages/Home/script.js
@@ -2,5 +2,20 @@ const menuIcon = document.getElementById("menu-icon");
 const menu = document.getElementById("menu");
 
 menuIcon.addEventListener("click", () => {
-    menu.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+document.querySelector(".dropbtn").addEventListener("click", function (e) {
+  e.preventDefault(); // Impede comportamento padrão
+  const dropdown = this.parentElement;
+  dropdown.classList.toggle("show");
+});
+
+// Fecha o dropdown se clicar fora
+window.addEventListener("click", function (e) {
+  if (!e.target.matches(".dropbtn")) {
+    document.querySelectorAll(".dropdown").forEach(function (dropdown) {
+      dropdown.classList.remove("show");
+    });
+  }
 });
